2e9a81a11c7c2bb6168fccd2c3920c682a3c67b3
Evan Bagwell <etb@wi.rr.com>
2021-06-23 15:10:10 -0500

    Quick Update

--- Barrels Final Project/CodeTheWay/CodeTheWay/CodeTheWay.Web.Ui/Repositories/BarrelRepository.cs
+++ Barrels Final Project/CodeTheWay/CodeTheWay/CodeTheWay.Web.Ui/Repositories/BarrelRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CodeTheWay.Web.Ui.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
