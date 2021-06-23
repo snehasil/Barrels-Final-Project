2aa089250b16eebc7de4b3e590e4551efe69b024
Om Rajpal <omrajpal15@gmail.com>
2021-06-23 15:04:38 -0500

    Repository + Interface Repository

---
+++ Barrels Final Project/CodeTheWay/CodeTheWay/CodeTheWay.Web.Ui/Repositories/IBarrelRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTheWay.Web.Ui.Repositories
+{
+    public interface IBarrelRepository
+    {
+    }
+}
